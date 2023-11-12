4ae5dbb7685076d1c28cb00707b6d039ec4653d6
Rajat Hemant Deore <rajat.72002@gmail.com>
2023-11-12 20:26:45 +0530

    score

--- CLI_APP/index.js
+++ CLI_APP/index.js
@@ -8,9 +8,20 @@ var readlinesync = require('readline-sync');
 // console.log(res);
 
 var age = readlinesync.question("Are you Older than 18 ?");
-
+var score = 0;
 if (age=='yes') {
     console.log('You are in');
+    score +=1;
 } else {
     console.log("Come later");
+    score -=1;
 }
+var age = readlinesync.question("Are you Ready ?");
+if (age=='yes') {
+    console.log('You are in');
+    score +=1;
+} else {
+    console.log("Come later");
+    score -=1;
+}
+console.log(score);

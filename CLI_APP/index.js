1ed0037129b7c6fd3755f37564b5adf5f42765f6
Rajat Hemant Deore <rajat.72002@gmail.com>
2023-11-16 14:46:24 +0530

    functions

--- CLI_APP/index.js
+++ CLI_APP/index.js
@@ -7,21 +7,44 @@ var readlinesync = require('readline-sync');
 // var res = 7 + num;
 // console.log(res);
 
-var age = readlinesync.question("Are you Older than 18 ?");
+// var age = readlinesync.question("Are you Older than 18 ?");
+// var score = 0;
+// if (age=='yes') {
+//     console.log('You are in');
+//     score +=1;
+// } else {
+//     console.log("Come later");
+//     score -=1;
+// }
+// var age = readlinesync.question("Are you Ready ?");
+// if (age=='yes') {
+//     console.log('You are in');
+//     score +=1;
+// } else {
+//     console.log("Come later");
+//     score -=1;
+// }
+// console.log(score);
+
+//function
+
+// function addtwonumbers(num1,num2){
+//     var sum = num1 + num2;
+//     return sum;
+// }
+
+// console.log(addtwonumbers(5,2));
+
+//parameterized func
 var score = 0;
-if (age=='yes') {
-    console.log('You are in');
-    score +=1;
-} else {
-    console.log("Come later");
-    score -=1;
+function answers(question,answers){
+    var que1 = readlinesync.question(question);
+    if (que1==answers) {
+        score+=1;
+    }else{
+        score -=1;
+    }
+    return score;
 }
-var age = readlinesync.question("Are you Ready ?");
-if (age=='yes') {
-    console.log('You are in');
-    score +=1;
-} else {
-    console.log("Come later");
-    score -=1;
-}
-console.log(score);
+ console.log(answers("Are you ok ? ","yes"));
+

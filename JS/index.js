2b2c0551c38a02f455515cd09fa422c7e66ad434
Rajat Hemant Deore <rajat.72002@gmail.com>
2023-12-16 15:52:10 +0530

    Javascript

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,9 @@
-var name = prompt("Enter name");
-var welcomemsg = "Hello " + name;
-alert(welcomemsg); 
+// var name = prompt("Enter name");
+// var welcomemsg = "Hello " + name;
+// alert(welcomemsg); 
+
+var btnTranslate = document.querySelector("#btn-translate");
+console.log(btnTranslate);
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked");
+})
